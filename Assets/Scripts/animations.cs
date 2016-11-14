3fbe60652f5244c9b8c7d8a9088451986a0c99b5
RainbowZephyr <darkfeathers9@gmail.com>
2016-11-14 16:42:31 +0200

    added project

---
+++ Assets/Scripts/animations.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animations : MonoBehaviour {
+
+	private Animator anim;
+
+	public float ver;
+	public float hor;
+	public float sprint;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		ver = Input.GetAxis ("Vertical");
+		hor = Input.GetAxis ("Horizontal");
+		Sprinting();
+	}
+
+	void FixedUpdate () {
+
+		anim.SetFloat ("Walk", ver);
+		anim.SetFloat ("Turn", hor);
+		anim.SetFloat ("Sprint", sprint);
+
+	}
+
+	void Sprinting () {
+	
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			sprint = 0.2f;
+		} else {
+			sprint = 0.0f;
+		}
+	
+	}
+
+}
